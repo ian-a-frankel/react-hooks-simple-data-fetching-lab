39a2d4e01152cce731357f1116b5c962ea8e68ff
Ian Frankel <ian.frankel@gmail.com>
2023-10-30 16:32:00 -0400

    loading then random dog

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [picture, setPicture] = useState(<p>Loading...</p>)
+    useEffect(() => {
+        fetch("http://dog.ceo/api/breeds/image/random")
+          .then((response) => response.json())
+          .then(data => {
+            setPicture(<img src={data.message}/>)
+            }
+          );
+      }, [setPicture])
+
+      return picture
+}
+
+export default App
